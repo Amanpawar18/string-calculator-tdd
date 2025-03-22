eb42b22deafae67bf4b9bcc1f976609a0ba97ade
Aman <amanpawar9718@gmail.com>
2025-03-22 11:26:12 +0530

    Add Code for single number.

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,5 +1,9 @@
 const add = require("./stringCalculator");
 
 test("returns 0 for an empty string", () => {
-  expect(add("")).toBe(0);
+    expect(add("")).toBe(0);
+});
+
+test("returns the number itself when a single number is provided", () => {
+    expect(add("1")).toBe(1);
 });
